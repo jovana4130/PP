98593e267e0262933ce77d7caef4e75b1434e8e4
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-21 15:17:37 +0100

    Upload folders and files

---
+++ Arrays/arrays.js
@@ -0,0 +1,36 @@
+/*Exersize 1.*/
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+console.log(months[5]);
+console.log(months[9]);
+console.log(months[0]);
+
+/*Exersize 2.*/
+
+var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log(days[6]);
+
+/*Exersize 3.*/
+
+var elements = [2, -4, 5, -2, -11];
+console.log(elements[1]);
+console.log(elements[3]);
+console.log(elements[4]);
+
+/*Exersize 4.*/
+
+var elements = [5, 15, -5, 20, 12, 18, 72, 14, 9];
+console.log(elements[0]%3===0);
+console.log(elements[1]%3===0);
+console.log(elements[2]%3===0);
+console.log(elements[3]%3===0);
+console.log(elements[4]%3===0);
+console.log(elements[5]%3===0);
+console.log(elements[6]%3===0);
+console.log(elements[7]%3===0);
+console.log(elements[8]%3===0);
+
+/*Exersize 5.*/
+
+array = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+console.log(elements);
