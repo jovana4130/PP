399363847273360369c16a27c9777fb0212655a3
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-23 14:34:16 +0100

    Constructor function Exam

---
+++ Object/MVC/exam_statistics/js/exam.js
@@ -0,0 +1,21 @@
+'use strict';
+
+class Exam extends Subject  {
+    constructor(subject, name, surname, grade) {
+        super(subject);
+        this.name = name;
+        this.surname = surname;
+        this.grade = grade;
+    }
+    getExamInfo() {
+        var firstLetter = this.name[0].toUpperCase() + this.surname[0].toUpperCase();
+        return 'Subject: ' + subject + 'Student: ' + firstLetter;
+    }
+    hasPassed() {
+        if (this.grade > 5 === "Passed") {
+            this.grade > 5 !== 'Failed';
+        };
+    }
+};
+
+
