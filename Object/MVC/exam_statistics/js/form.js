fef05d5cb72a7f105d94524f88a34b3b94109b1e
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-23 15:45:33 +0100

    Trying to resolve the Form

--- Object/MVC/exam_statistics/js/form.js
+++ Object/MVC/exam_statistics/js/form.js
@@ -4,5 +4,45 @@ class Form extends Exam {
     constructor (subject, name, surname, grade) {
         super(subject, name, surname, grade);
     }
-    if ()
+/*    if ( document.Form.name.value === ' ')
+        return 'Please provide your Name and Surname';
+};*/
+
+var nameInputValue = inputNameElement.value;
+
+    if (nameInputValue === ' ')
+        inputNameElement.textContent = 'Please provide your Name and Surname';
+        return;
+    };
+
+
+
+
+var dateInputValue = inputDateElement.value;
+
+
+var Student = new Student();
+
+var addStudentErrorElement = document.querySelector('#add-student-error');
+var addSubjectErrorElement = document.querySelector('#add-subject-error');
+var addGradeErrorElement = document.querySelector('#add-grade-error');
+
+function newStudent() {
+    var subjectValue = inputSubjectElement.value;
+    var nameValue = inputNameElement.value;
+    var gradeValue = inputGradeElement.value;
+
+    if (!subjectValue) {
+        inputSubjectElement.textContent = 'Please choose the Subject, from the drop down menu.';
+        return;
+    }
+    if (!nameValue) {
+        inputNameElement.textContent = 'Please provide your Name and Surname.';
+        return;
+    }
+    if (!gradeValue) {
+        inputGradeElement.textContent = 'Please choose the Grade, from the drop down menu.';
+        return;
+    }
+
 }
