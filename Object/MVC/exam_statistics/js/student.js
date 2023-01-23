6079ce4d9b608f0fc8a1cb79de94d198f1125691
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-23 14:09:03 +0100

    Changed prototype to class

--- Object/MVC/exam_statistics/js/student.js
+++ Object/MVC/exam_statistics/js/student.js
@@ -1,5 +1,20 @@
-class Person {
-    constructor(name) {
+/*function Student(name, surname) {
+    this.name = name;
+    this.surname = surname;
+};
+
+Student.prototype.getStudentData = function () {
+    var firstLetter = this.name[0].toUpperCase() + this.surname[0].toUpperCase();
+    return 'Student: ' + firstLetter;
+};*/
+
+class Student {
+    constructor(name, surname) {
         this.name = name;
+        this.surname = surname;
+    }
+    getStudentData() {
+        var firstLetter = this.name[0].toUpperCase() + this.surname[0].toUpperCase();
+        return 'Student: ' + firstLetter;
     }
 };
