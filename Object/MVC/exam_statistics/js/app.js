7adb48d8d8f88e22d6022c865a6184545086d819
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-24 19:34:28 +0100

    Trying to resolve app.js

--- Object/MVC/exam_statistics/js/app.js
+++ Object/MVC/exam_statistics/js/app.js
@@ -1,1 +1,39 @@
 'use strict';
+
+var studentCount = 0;
+var passedCount = 0;
+var failedCount = 0;
+
+var selectSubject = document.querySelector("#subjects");
+var newStudent = document.querySelector("#name");
+var addEntry = document.querySelector("#add-entry");
+var entryError = document.querySelector("#entry-error");
+var chooseGrade = document.querySelector("#grades");
+
+var selectSubject = document.querySelector("#subjects");
+var newStudent = document.querySelector("#name");
+var addEntry = document.querySelector("#add-entry");
+var entryError = document.querySelector("#entry-error");
+var chooseGrade = document.querySelector("#grades");
+
+var subjectPassed = document.querySelector("#subject-passed");
+var subjectFailed = document.querySelector("#subject-failed");
+
+if (grade >= 6) {
+    passedCount++;
+    passedCountSpan.textContent = passedCount;
+    passedList.appendChild(newItem);
+} else {
+    failedCount++;
+    failedCountSpan.textContent = failedCount;
+    failedList.appendChild(newItem);
+};
+
+
+if(failedCount === 0){
+    failedCountSpan.textContent = "0";
+    percentageFailed.textContent = "0%";
+} else {
+    var percentage = (failedCount / studentCount) * 100;
+    percentageFailed.textContent = percentage.toFixed(0) + "%";
+};
