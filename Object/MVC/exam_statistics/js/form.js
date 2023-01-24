41f2a3d60942994d50e2fdd36306388a28ae1fce
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-24 19:32:15 +0100

    Trying to resolve form.js

--- Object/MVC/exam_statistics/js/form.js
+++ Object/MVC/exam_statistics/js/form.js
@@ -1,6 +1,6 @@
 'use strict';
 
-class Form extends Exam {
+/*class Form extends Exam {
     constructor (subject, name, surname, grade) {
         super(subject, name, surname, grade);
     }
@@ -12,21 +12,17 @@ class Form extends Exam {
 
 function collectAllData() {
 
-};
+};*/
+
 /*    if ( document.Form.name.value === ' ')
         return 'Please provide your Name and Surname';
 };*/
 
-var nameInputValue = inputNameElement.value;
+/*var nameInputValue = inputNameElement.value;
 
     if (nameInputValue === ' ') 
         inputNameElement.textContent = 'Please provide your Name and Surname';
         return;
-
-
-
-
-var dateInputValue = inputDateElement.value;
 
 
 var Student = new Student();
@@ -53,4 +49,27 @@ function newStudent() {
         return;
     }
 
+};*/
+
+var subjectValue = inputSubjectElement.value;
+var nameValue = inputNameElement.value;
+var gradeValue = inputGradeElement.value;
+
+if (!subjectValue) {
+    inputSubjectElement.textContent = 'Please choose the Subject, from the drop down menu.';
+    return;
+}
+if (!nameValue) {
+    inputNameElement.textContent = 'Please provide your Name and Surname.';
+    return;
+}
+if (!gradeValue) {
+    inputGradeElement.textContent = 'Please choose the Grade, from the drop down menu.';
+    return;
 };
+
+studentCount++;
+totalStudentCount.textContent = studentCount;
+
+var newItem = document.createElement("li");
+newItem.textContent = subject + " - " + student + "                              " + grade;
