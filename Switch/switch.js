c0eebd4f5690e08ec0366362581d16bbb62d286f
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-25 19:09:40 +0100

    Exercises Switch  upload

--- Switch/switch.js
+++ Switch/switch.js
@@ -1,6 +1,10 @@
-/*Exercise 1 and 2*/
+/*Exercise 1. 
+Write a program that shows text representation of a day in a week for a number input from
+1 to 7. Print output in console.
+For input 1, output should be “Monday”. */
 
-var day = 100;
+var day = 1;
+//var result = '';
 
 switch(day) {
     case 1:
@@ -26,8 +30,49 @@ switch(day) {
         break;
     default:
         day = 'Input must be a number between 1 and 7';
-}
+};
 console.log(day);
+//console.log(result);
+
+
+/*Exercise 2.
+ Write a program that shows text representation of a day in a week for a number input from
+1 to 7. All other cases output a message explaining that input must be a number between 1
+and 7.
+For input 1, output should be “Monday”.
+For input 10, output should be “Input must be a number between 1 and 7”.*/
+
+var day = 10;
+//var result = '';
+
+switch(day) {
+    case 1:
+        day = 'Monday';
+        break;
+    case 2:
+        day = 'Tuesday';
+        break;
+    case 3: 
+        day = 'Wednesday';
+        break;
+    case 4:
+        day = 'Thursday';
+        break; 
+    case 5:
+        day = 'Friday';
+        break;
+    case 6:
+        day = 'Saturday';
+        break;
+    case 7:
+        day = 'Sunday';
+        break;
+    default:
+        day = 'Input must be a number between 1 and 7';
+};
+console.log(day);
+//console.log(result);
+
 
 /*Exercise 3*/
 
