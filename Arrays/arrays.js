29178a995e750d869529280d78548ebca471489e
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-25 18:43:48 +0100

    Exersizes text update

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,23 +1,29 @@
-/*Exersize 1.*/
+/*Exersize 1. 
+Write an array of months in a year. Using console.log display June, October and January
+from the array.*/
 
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 console.log(months[5]);
 console.log(months[9]);
 console.log(months[0]);
 
-/*Exersize 2.*/
+/*Exersize 2. 
+Write an arrays of days in a week. Using console.log display Sunday from the array. */
 
 var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 console.log(days[6]);
 
-/*Exersize 3.*/
+/*Exersize 3. 
+Print all negative elements from the array [2, -4, 5, -2, -11].*/
 
 var elements = [2, -4, 5, -2, -11];
 console.log(elements[1]);
 console.log(elements[3]);
 console.log(elements[4]);
 
-/*Exersize 4.*/
+/*Exersize 4. 
+Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18,
+72, 14, 9].*/
 
 var elements = [5, 15, -5, 20, 12, 18, 72, 14, 9];
 console.log(elements[0]%3===0);
@@ -30,7 +36,8 @@ console.log(elements[6]%3===0);
 console.log(elements[7]%3===0);
 console.log(elements[8]%3===0);
 
-/*Exersize 5.*/
+/*Exersize 5. 
+What is the index of number 24 in the following array?*/
 
 array = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
 console.log(elements);
