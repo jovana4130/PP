548dccce6864190818fba20139043f6e9403b267
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-25 19:40:50 +0100

    Exercises Loops2 update

--- Loops/loops-exercises2.js
+++ Loops/loops-exercises2.js
@@ -1,4 +1,5 @@
-/*1. Write a program that checks if a given element e is in the array a.
+/* Exercises 1. 
+Write a program that checks if a given element e is in the array a.
 Input: e = 3, a = [5, -4.2, 3, 7]
 Output: yes
 Input: e = 3, a = [5, -4.2, 18, 7]
@@ -16,7 +17,8 @@ for (i = 0; i < a.length; i++) {
 }
 
 
-/*2. Write a program that multiplies every positive element of a given array by 2.
+/* Exercises 2. 
+Write a program that multiplies every positive element of a given array by 2.
 Input array: [-3, 11, 5, 3.4, -8]
 Output array: [-3, 22, 10, 6.8, -8] */
 
@@ -30,7 +32,8 @@ for (var i = 0; i < a.length; i++) {
 console.log(a);
 
 
-/*3. Write a program that finds the minimum of a given array and prints out its value and
+/* Exercises 3. 
+Write a program that finds the minimum of a given array and prints out its value and
 index.
 Input array: [4, 2, 2, -1, 6]
 Output: -1, 3 */
@@ -48,7 +51,8 @@ console.log(minimum +', ' + index);
 
 
 
-/*4. Write a program that finds the first element larger than minimum and prints out its value.
+/* Exercises 4. 
+Write a program that finds the first element larger than minimum and prints out its value.
 Input array: [4, 2, 2, -1, 6]
 Output: 2 */
 
@@ -70,7 +74,8 @@ for (var i = 0; i < a.length; i++) {
 }
 console.log(newminimum);
 
-/* 5. Write a program that calculates the sum of positive elements in the array.
+/* Exercises 5. 
+Write a program that calculates the sum of positive elements in the array.
 Input array: [3, 11, -5, -3, 2]
 Output: 16*/
 
@@ -85,7 +90,8 @@ for (var i = 0; i < a.length; i++) {
 console.log(sumpositive);
 
 
-/*6. Write a program that checks if a given array is symmetric. An array is symmetric if it can
+/* Exercises 6. 
+Write a program that checks if a given array is symmetric. An array is symmetric if it can
 be read the same way both from the left and the right hand side.
 Input array: [2, 4, -2, 7, -2, 4, 2]
 Output: The array is symmetric.
@@ -118,7 +124,8 @@ console.log(result);
 
 
 
-/*7. Write a program that intertwines two arrays. You can assume the arrays are of the same
+/* Exercises 7. 
+Write a program that intertwines two arrays. You can assume the arrays are of the same
 length.
 Input arrays: [4, 5, 6, 2], [3, 8, 11, 9]
 Output array: [4, 3, 5, 8, 6, 11, 2, 9] */
@@ -134,7 +141,8 @@ for (var i = 0; i < a.length; i++) {
 console.log(c);
 
 
-/*8. Write a program that concatenates two arrays.
+/* Exercises 8. 
+Write a program that concatenates two arrays.
 Input arrays: [4, 5, 6, 2], [3, 8, 11, 9]
 Output array: [4, 5, 6, 2, 3, 8, 11, 9]*/
 
@@ -160,7 +168,8 @@ var c = [a + ',' + b];
 
 console.log(c);
 
-/*9. Write a program that deletes a given element e from the array a.
+/* Exercises 9. 
+Write a program that deletes a given element e from the array a.
 Input: e = 2, a = [4, 6, 2, 8, 2, 2] */
 
 var e = 2;
@@ -175,7 +184,8 @@ for (var i = 0; i < a.length; i++) {
 console.log(a);
 
 /*Output array: [4, 6, 8]
-10. Write a program that inserts a given element e on the given position p in the array a. If
+Exercises 10. 
+Write a program that inserts a given element e on the given position p in the array a. If
 the value of the position is greater than the array length, print the error message.
 Input: e = 78, p = 3, a = [2, -2, 33, 12, 5, 8]
 Output: [2, -2, 33, 78, 12, 5, 8] */
