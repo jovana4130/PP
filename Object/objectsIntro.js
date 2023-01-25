2ca8f0e80986cbc31200b3b46efae2bcc4cc9860
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-25 20:06:04 +0100

    Exercises Object intro update

--- Object/objectsIntro.js
+++ Object/objectsIntro.js
@@ -1,4 +1,5 @@
-/*1. Create an object that represents your favourite coffee. Please include coffee name,
+/* Exercise 1. 
+Create an object that represents your favourite coffee. Please include coffee name,
 strength, flavour, milk, sugar, … everything you like!*/
 
 var coffee = {
@@ -7,12 +8,12 @@ var coffee = {
     flavour: 'vanilla',
     milk: 'soy',
     sugar: 'stevia', 
-}
-
+};
 console.log(coffee.name);
 
 
-/*2. Create an object that represents your favourite movie. Please include title, actors,
+/* Exercise 2. 
+Create an object that represents your favourite movie. Please include title, actors,
 director, genre, popularity.*/
 
 var favMovie = {
@@ -26,7 +27,8 @@ var favMovie = {
 console.log(favMovie.director);
 
 
-/*3. Write a function that creates an object that represents a project. Each project is
+/*Exercise 3. 
+Write a function that creates an object that represents a project. Each project is
 described by: description, programming language, git repository, boolean status that
 says if the project is in development or not. Add a method that prints out the project&#39;s
 repository, a method that checks if the project is written in JavaScript as well as a
@@ -88,7 +90,8 @@ console.log(p("Environment Quality Improval", "PHP", "fsdfdf.github.com", true))
 
 
 
-/*4. Write a function that creates an object that represents a culinary recipe. Each recipe is
+/* Exercise 4. 
+Write a function that creates an object that represents a culinary recipe. Each recipe is
 described by: name, type of cuisine, complexity (value from 1 to 5), list of ingredients,
 preparing time, preparing instruction.
 ○ Add a method that prints out all the ingredients necessary for the meal
