cfd625c1391b3d23aa6a92c62268a55e92721b62
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-25 19:25:40 +0100

    Exercises Loops1 update

--- Loops/loops-exercises1.js
+++ Loops/loops-exercises1.js
@@ -1,8 +1,13 @@
+/* Exercises 0.
+Write a program that will iterate from 0 to 10 and display squares of numbers.*/
+var x = 0;
+
 for (var x = 0; x <= 10; x++) {
     console.log(x * x);
-}
+};
 
-/* 1. Write a for loop that will iterate from 0 to 15. For each iteration, it will check if the current
+/* Exercises 1. 
+Write a for loop that will iterate from 0 to 15. For each iteration, it will check if the current
 number is odd or even, and display a message to the screen. */
 
 for (var i=0; i<=15; i++) {
