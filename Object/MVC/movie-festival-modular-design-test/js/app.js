322d62cc3f0d79e6fb6f185701143f76fbdb5f88
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-28 12:16:15 +0100

    Movie festival modular design inital push

---
+++ Object/MVC/movie-festival-modular-design-test/js/app.js
@@ -0,0 +1,29 @@
+var mainModule = (function (ui, data) {
+    var createMovieButton = document.querySelector('#create-movie'),
+    createProgramButton = documetn.querySelector('#add-movie'),
+    addMovieToProgramButton = document.querySelector('#add-movie');
+
+    createMovieButton.addEventListener('click', function () {
+    });
+
+    createProgramButton.addEventListener('click', function () {
+        var formData = ui.collectProgramData();
+        var isValid = data.isValidProgram(formData.date);
+        if (!isValid) {
+            ui.setProgramError('Invalid input');
+            return;
+        }
+
+        
+    });
+
+    addMovieToProgramButton.addEventListener('click', function () {
+        var formData = ui.collectAddMovieFormData();
+        var updatedProgram = data.addMovieToProgram(
+            formData.movieIndex,
+            formData.programIndex
+    });
+
+}
+
+)
---
+++ Object/MVC/movie-festival-modular-design-test/js/ui.js
@@ -0,0 +1,42 @@
+'use strict'
+
+selectGenreElement = document.querySelector('#genre');
+movieSelectElement = document.querySelector('#movie-select');
+ulMovieListElement = document.querySelector('#movie-list');
+inputDateElement = document.querySelector('#date');
+programErrorElement = document.querySelector('#program-error');
+ulProgramListElement = document.querySelector('#program-list');
+movieToProgramErrorElement = document.querySelector('#movie-to-program-error'),
+programSelectElement = document.querySelector('#program-select');
+
+function collectMovieData(){
+
+}
+
+function collectProgramData() {
+
+}
+
+function setProgramError(error) {
+
+}
+
+function updateProgramData(program, oldProgramData) {
+
+}
+
+return {
+    collectMovieData: collectMovieData,
+    setMovieError: setMovieError,
+    updateMovieList: updateMovieList,
+    clearInputs: clearInputs,
+    collectProgramData: collectProgramData,
+    setProgramError: setProgramError,
+    updateProgramList: updateProgramList,
+    collectAddMovieFormData: collectAddMovieFormData,
+    setMovieToProgramError: setMovieToProgramError,
+    updateProgramData: updateProgramData,
+    };
+})();
+
+
