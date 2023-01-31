6b2de3483d1cc7950c10abc97b9648e4900ebcc4
Jovana Vucetic <jovana@Jovanas-MacBook-Air.local>
2023-01-31 19:09:02 +0100

    New class new update ui.js

--- Object/MVC/movie-festival-modular-design-test/js/ui.js
+++ Object/MVC/movie-festival-modular-design-test/js/ui.js
@@ -2,16 +2,16 @@
 
 var uiModule = (function () {
     var movieErrorElement = document.querySelector('movie-error');
-    inputTitleElement = document.querySelector('title');
-    inputLenghtElement = document.querySelector('length');
-    selectGenreElement = document.querySelector('#genre');
-    movieSelectElement = document.querySelector('#movie-select');
-    ulMovieListElement = document.querySelector('#movie-list');
-    inputDateElement = document.querySelector('#date');
-    programErrorElement = document.querySelector('#program-error');
-    ulProgramListElement = document.querySelector('#program-list');
-    movieToProgramErrorElement = document.querySelector('#movie-to-program-error'),
-    programSelectElement = document.querySelector('#program-select');
+    var inputTitleElement = document.querySelector('title');
+    var inputLenghtElement = document.querySelector('length');
+    var selectGenreElement = document.querySelector('#genre');
+    var ulMovieListElement = document.querySelector('#movie-list');
+    var movieSelectElement = document.querySelector('#movie-select');
+    var inputDateElement = document.querySelector('#date');
+    var programErrorElement = document.querySelector('#program-error');
+    var ulProgramListElement = document.querySelector('#program-list');
+    var movieToProgramErrorElement = document.querySelector('#movie-to-program-error');
+    var programSelectElement = document.querySelector('#program-select');
 
     function collectMovieData(){
         var titleValue = inputTitleElement.value;
@@ -54,13 +54,26 @@ var uiModule = (function () {
         };
     };
 
-    function setMovieToProgramError(error) {
+    /*function setMovieToProgramError(error) {
 
-    }
+    }*/
 
     function setProgramError(error) {
 
-    }
+    } 
+
+    function updateProgramList(program, index) {
+        var index = festival.programList.length - 1;
+        var li = document.createElement("li");
+        li.setAttribute("id", "program-item-" + index);
+        li.textContent = program.getData();
+        ulProgramListElement.appendChild(li);
+    
+        var option = document.createElement("option");
+        option.setAttribute("value", index);
+        option.textContent = program.getData();
+        selectProgramElement.appendChild(option);
+    };
 
     function updateProgramData(program, oldProgramData) {
         var programListElements = document.querySelectorAll('#program-list li');
@@ -93,30 +106,4 @@ var uiModule = (function () {
         updateProgramData: updateProgramData,
         };
     };
-
-    function updateProgramList() {
-        var index = festival.programList.length - 1;
-        var li = document.createElement("li");
-        li.setAttribute("id", "program-item-" + index);
-        li.textContent = program.getData();
-        ulProgramListElement.appendChild(li);
-    
-        var option = document.createElement("option");
-        option.setAttribute("value", index);
-        option.textContent = program.getData();
-        selectProgramElement.appendChild(option);
-    };
-
-    return {
-        collectMovieData: collectMovieData,
-        setMovieError: setMovieError,
-        updateMovieList: updateMovieList,
-        clearInputs: clearInputs,
-        collectProgramData: collectProgramData,
-        setProgramError: setProgramError,
-        updateProgramList: updateProgramList,
-        collectAddMovieFormData: collectAddMovieFormData,
-        setMovieToProgramError: setMovieToProgramError,
-        updateProgramData: updateProgramData,
-        };
 })();
